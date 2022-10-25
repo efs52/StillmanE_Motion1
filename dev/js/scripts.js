7c4a4bd8f7a38e845d0eb6157049bcf505307450
efs52 <efs52@drexel.edu>
2022-10-25 00:09:00 -0400

    a lot

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -66,6 +66,59 @@ function aboutAnimation(){
     return tl;
 }
 
+
+
+
+
+
+
+// console.clear();
+
+// gsap.registerPlugin(ScrollTrigger, DrawSVGPlugin, MotionPathPlugin);
+// gsap.defaults({ease: "none"});
+
+
+
+// const pulses = gsap.timeline({
+//   defaults: {
+//     duration: 0.05, 
+//     autoAlpha: 1, 
+//     scale: 2, 
+//     transformOrigin: 'center', 
+//     ease: "elastic(2.5, 1)"
+//   }})
+// .to(".ball02, .text01", {}, 0.2) 
+// .to(".ball03, .text02", {}, 0.33)
+// .to(".ball04, .text03", {}, 0.46)
+
+// const main = gsap.timeline({defaults: {duration: 1},
+//   scrollTrigger: {
+//     trigger: "#svg",
+//     scrub: true,
+//     start: "top center",
+//     end: "bottom center"
+//   }})
+// .to(".ball01", {duration: 0.01, autoAlpha: 1})
+// .from(".theLine", {drawSVG: 0}, 0)
+// .to(".ball01", {motionPath: {
+//   path: ".theLine", 
+//   align:".theLine",
+//   alignOrigin: [0.5, 0.5],
+// }}, 0)
+// .add(pulses, 0);
+
+
+
+
+
+
+
+
+
+
+
+
+
 function wonderAnimation(){
     var tl =gsap.timeline({scrollTrigger:{trigger:"#hero-2", scrub:true, end:"top 40%", start:"top 80%"}});
     tl.from("#bg-img",{duration:5, clipPath:"inset(0 50%)"})
