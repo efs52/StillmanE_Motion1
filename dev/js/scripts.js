f6cb54e13a74c87d25fe7fd810ba53c4d5f9f645
efs52 <efs52@drexel.edu>
2022-11-03 03:51:56 -0400

    draw svg ish

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,52 +1,19 @@
 import { gsap } from "gsap";
 import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { TimelineMax } from "gsap/gsap-core";
 import { GSDevTools } from "gsap/GSDevTools";
 
 gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
 
 function simpleMotion(){
+    var tl = gsap.timeline()
+    tl.from("#line", {drawSVG:"0%", duration: 3});
 
-  var tl = gsap.timeline();
-  tl.fromTo("#vectorline", 1, {drawSVG:0, x: vectorline.width, delay: 0.5});
+    return tl;
 
-  return tl;
+  }
 
-  
-
-  // var vectorline = document.querySelector("#vectorline");
-  
-  // vectorline = vectorline.getBBox();
-
-  // var tl = gsap.timeline();
-  // tl.fromTo("#vectorline", 1, {drawSVG:0, x: vectorline.width, delay: 0.5});
-
-  // return tl;
-
-
-
-
-    // var line = document.querySelector("#center-line");
-
-    // line = line.getBBox();
-
-    // var leftBall = document.querySelector("#left-ball");
-    // leftBall = leftBall. getBBox();
-
-    // var center = document.querySelector("#center");
-    // center = center.getBBox();
-    // console.log(center.height)
-
-    // gsap.set("#left-ball", {x:line.width / 2 + leftBall.width / 2, transformOrigin: "center"})
-    // gsap.set("#right-ball", {x:line.width / 2 - leftBall.width / 2, transformOrigin: "center"})
-
-    // var tl = gsap.timeline();
-    // tl.from("#right-ball", {scale:0, duration: 0.25, drawSVG: 0})
-    //   .from("#left-ball", {scale:0, duration:0.25},"-+50%")
-    //   .to("#")
-
-    // return tl;
-}
 
 function patternMotion(){
 
@@ -72,10 +39,30 @@ function patternMotion(){
 //      return tl;
 // }
 
+function Preloader(){
+
+  var tl = new
+  TimelineMax ()
+  tl.from("#bigsquare", {duration: 2, rotate: 180, yoyoEase: "power2.out", transformOrigin: "center", repeat: 2, repeatDelay:0.5})
+  .set("#leftarrow", {duration: 0.5, xPercent: -30})
+  .from("#rightarrow", {duration: 0.5, xPercent: -30})
+
+  return tl;
+
+
+  // var tl = gsap.timeline()
+  // tl.to("#bigsquare", {duration: 2, rotate: 180, yoyoEase: "power2.out", transformOrigin: "center", repeat: 5, repeatDelay:0.5})
+  // .to("#leftarrow", {duration: 0.5, xPercent: -30})
+  // .to("#rightarrow", {duration: 0.5, xPercent: 30});
+  // return tl;
+
+
+}
+
 
 var mainTL = gsap.timeline();
 mainTL.add(simpleMotion())
    .add(patternMotion())
-  //  .add(UIMotion())
+   .add(Preloader())
 
    GSDevTools.create();
