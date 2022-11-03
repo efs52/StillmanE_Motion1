c710d151315cab10880e2f7ae4af6124b35a51dd
efs52 <efs52@drexel.edu>
2022-11-02 22:47:40 -0400

    yer

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,6 +7,25 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
 function simpleMotion(){
 
+  var tl = gsap.timeline();
+  tl.fromTo("#vectorline", 1, {drawSVG:0, x: vectorline.width, delay: 0.5});
+
+  return tl;
+
+  
+
+  // var vectorline = document.querySelector("#vectorline");
+  
+  // vectorline = vectorline.getBBox();
+
+  // var tl = gsap.timeline();
+  // tl.fromTo("#vectorline", 1, {drawSVG:0, x: vectorline.width, delay: 0.5});
+
+  // return tl;
+
+
+
+
     // var line = document.querySelector("#center-line");
 
     // line = line.getBBox();
@@ -33,30 +52,30 @@ function patternMotion(){
 
     var tl = gsap.timeline();
 
-    tl.from(".odd", {duration:3, drawSVG:0, stagger:0.25, rotate:180, transformOrigin: "center"}, "spin")
-    .fromTo(".even", {drawSVG: "0%, 0%"}, {duration:2, drawSVG:"0%, -100%", stagger:0.25, rotate:180, transformOrigin: "center"}, "spin");
+    tl.from(".small", {duration:4, drawSVG:0, stagger:0.25, rotate:180, transformOrigin: "center"}, "spin")
+    .fromTo(".big", {drawSVG: "0%, 0%"}, {duration:3, drawSVG:"0%, -100%", stagger:0.25, rotate:180, transformOrigin: "center"}, "spin");
 
     return tl;
 }
 
-function UIMotion(){
-     var bottomLine = document.querySelector("#bottom-line");
+// function UIMotion(){
+//      var bottomLine = document.querySelector("#bottom-line");
 
-     bottomLine = bottomLine.getBBox();
+//      bottomLine = bottomLine.getBBox();
 
-     console.log(bottomLine.width);
-     var tl = gsap.timeline();
-     tl.to("#pencil", {duration: 0.5, x: bottomLine.width}, "draw")
-     .from("#bottom-line", {duration: 0.5, drawSVG:0}, "draw")
-     .to("#pencil", {duration: 0.5, alpha:0}, "drawOutline")
-     .fromTo("#outline", {drawSVG:"100% 100%"},{duration: 0.5, drawSVG:"100% 0%"}, "drawOutline")
-     return tl;
-}
+//      console.log(bottomLine.width);
+//      var tl = gsap.timeline();
+//      tl.to("#pencil", {duration: 0.5, x: bottomLine.width}, "draw")
+//      .from("#bottom-line", {duration: 0.5, drawSVG:0}, "draw")
+//      .to("#pencil", {duration: 0.5, alpha:0}, "drawOutline")
+//      .fromTo("#outline", {drawSVG:"100% 100%"},{duration: 0.5, drawSVG:"100% 0%"}, "drawOutline")
+//      return tl;
+// }
 
 
 var mainTL = gsap.timeline();
 mainTL.add(simpleMotion())
    .add(patternMotion())
-   .add(UIMotion())
+  //  .add(UIMotion())
 
    GSDevTools.create();
