9019f9e3226c0e2a31adc9bf4219717927e62046
efs52 <efs52@drexel.edu>
2022-11-22 19:09:57 -0500

    whatever

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -11,28 +11,33 @@ import { GSDevTools } from "gsap/GSDevTools";
 // tl.to("#dot2", {y: 50, duration: 1});
 // tl.to("#dot3", {opacity: 0, duration: 1});
 
+// var tl = gsap.timeline();
+// tl.to(".class1", {rotation: -270, duration: 1, ease: "elastic"})
+//   .to(".class2", {rotation: -360, duration: 1, ease: "elastic"})
+//   .to(".class3", {rotation: -180, duration: 1, ease: "elastic"});
+
 
 var tl = gsap.timeline({ duration: 0.5, ease: 'none', opacity: 0 });
 tl.to("#dot1", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
-tl.to("#dot2", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
-tl.to("#dot3", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
-tl.to("#dot4", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
-tl.to("#dot5", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
-tl.to("#dot6", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
-tl.to("#dot7", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
-tl.to("#dot8", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
-tl.to("#dot9", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
-tl.to("#dot10", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
-tl.to("#dot11", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
-tl.to("#dot12", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
-tl.to("#dot13", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
-tl.to("#dot14", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
-tl.to("#dot15", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
-tl.to("#dot16", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
-tl.to("#dot17", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
-tl.to("#dot18", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
-tl.to("#dot19", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
-tl.to("#dot20", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
+  .to("#dot2", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
+  .to("#dot3", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
+  .to("#dot4", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
+  .to("#dot5", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
+  .to("#dot6", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
+  .to("#dot7", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
+  .to("#dot8", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
+  .to("#dot9", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
+  .to("#dot10", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
+  .to("#dot11", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
+  .to("#dot12", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
+  .to("#dot13", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
+  .to("#dot14", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
+  .to("#dot15", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
+  .to("#dot16", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
+  .to("#dot17", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
+  .to("#dot18", 2.3, {autoAlpha: 1}, {autoAlpha: 0})
+  .to("#dot19", 2.3, {autoAlpha: 0}, {autoAlpha: 1})
+  .to("#dot20", 2.3, {autoAlpha: 1}, {autoAlpha: 0});
 
 
     // .from('#line-1', { y: 100 }, '-=0.2')
