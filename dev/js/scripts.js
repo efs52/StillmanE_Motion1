ac3693d90966eae8a3cbf00f5ab97a62a88430c9
efs52 <efs52@drexel.edu>
2022-12-03 21:13:07 -0500

    yup

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,4 +1,5 @@
 import { gsap } from "gsap";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
 import { GSDevTools } from "gsap/GSDevTools";
 
 gsap.registerPlugin(GSDevTools, DrawSVGPlugin);
@@ -14,18 +15,36 @@ function CameraBody(){
 function Rectangle(){
     var tl = gsap.timeline()
         tl.from("#line_through_camera", {x: -600, duration: 0.75})
-          .to("#line_through_camera", {delay: 4, x: 20})
+          .to("#line_through_camera", {x: 20})
 
+    return tl;
+}
+
+function CameraLensCenter(){
+    var tl = gsap.timeline()
+        tl.from("#inner_circle", {scale:0.3, transformOrigin: 'center', duration:0.25});
     return tl;
 }
 
 function CameraLensStroke(){
     var tl = gsap.timeline()
-        tl.from("#outer_circle", {drawSVG: "0%, 0%"});
-
-
+        tl.from("#outer_circle", {drawSVG: "0%, 0%", duration:0.75});
     return tl;
 }
+
+function CameraButton(){
+    var tl = gsap.timeline()
+        tl.fromTo("#camera_button", {display:'none'}, {display:'inline', duration: 2, y:-1})
+    return tl;
+}
+
+function CameraFlash1(){
+    var tl = gsap.timeline()
+        tl.from("#small_cam_flash", {y:100})
+    return tl;
+}
+
+
 
 
 
@@ -34,7 +53,10 @@ function CameraLensStroke(){
 var mainTimeline = gsap.timeline();
 mainTimeline.add(CameraBody())
      .add(Rectangle())
-     .add(CameraLensStroke());
+     .add(CameraLensCenter())
+     .add(CameraLensStroke())
+     .add(CameraButton())
+     .add(CameraFlash1());
 
 
 
